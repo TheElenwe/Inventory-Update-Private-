ee9f6a0b496ec1c9d59c2be591f29c1292d0f83f
unknown <you@example.com>
2021-10-15 22:30:19 +0300

    first commit

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-function updateInventory(arr1, arr2) {
-  for (let i = 0; i < arr2.length; i++) {
-    let inventoryExist = arr1.every(function (element) {
-      return arr2[i][1] != element[1];
-    });
-    if (inventoryExist) {
-      arr1.push(arr2[i]);
-    } else {
-      let indexVal = arr1.findIndex(function (element) {
-        return element[1] === arr2[i][1];
-      });
-      arr1[indexVal][0] += arr2[i][0];
-    }
+function sym() {
+  let args = [];
+  for (let i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
   }
-  return arr1.sort(function (a, b) {
-    a[1] < b[2] ? -1 : 1;
-  });
+
+  function symDiff(arrayOne, arrayTwo) {
+    let result = [];
+
+    arrayOne.forEach(function (item) {
+      if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    });
+
+    arrayTwo.forEach(function (item) {
+      if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    });
+
+    return result;
+  }
+
+  // Apply reduce method to args array, using the symDiff function
+  return args.reduce(symDiff);
 }
-
-var curInv = [
-  [21, "Bowling Ball"],
-  [2, "Dirty Sock"],
-  [1, "Hair Pin"],
-  [5, "Microphone"],
-];
-
-var newInv = [
-  [2, "Hair Pin"],
-  [3, "Half-Eaten Apple"],
-  [67, "Bowling Ball"],
-  [7, "Toothpaste"],
-];
-
-console.log(updateInventory(curInv, newInv));
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
